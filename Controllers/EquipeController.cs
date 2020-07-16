86c2738a1c49942cd5ecebefc079199972e5f062
Gabriel Ferreira <you@example.com>
2020-07-15 23:22:20 -0300

    Logo de equipe e excluir

--- Controllers/EquipeController.cs
+++ Controllers/EquipeController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using E_Players37_AspNetCore.Models;
 using Microsoft.AspNetCore.Http;
+using System.IO;
 
 namespace E_Players37_AspNetCore.Controllers
 {
@@ -23,11 +24,43 @@ namespace E_Players37_AspNetCore.Controllers
             Equipe equipeModel   = new Equipe();
             equipeModel.IdEquipe = Int32.Parse(form["IdEquipe"]);
             equipeModel.Nome     = form["Nome"];
+
+            // Upload Início
+            var file    = form.Files[0];
+            var folder  = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/Equipes");
+
+            if(file != null)
+            {
+                if(!Directory.Exists(folder)){
+                    Directory.CreateDirectory(folder);
+                }
+
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/", folder, file.FileName);
+                using (var stream = new FileStream(path, FileMode.Create))  
+                {  
+                    file.CopyTo(stream);  
+                }
+                equipeModel.Imagem   = file.FileName;
+            }
+            else
+            {
+                equipeModel.Imagem   = "padrao.png";
+            }
+            // Upload Final
+
             equipeModel.Imagem   = form["Imagem"];
 
             equipeModel.Create(equipeModel);            
             ViewBag.Equipes = equipeModel.ReadAll();
 
+            return LocalRedirect("~/Equipe");
+        }
+
+        [Route("Equipe/{id}")]
+        public IActionResult Excluir(int id)
+        {
+            equipeModel.Delete(id);
+            ViewBag.Equipes = equipeModel.ReadAll();
             return LocalRedirect("~/Equipe");
         }
     }
